92fb478977e70538507a71c7f3e9b83e46388d73
Walkire <allvinn@mail.gvsu.edu>
2016-12-15 19:57:21 -0500

    added gitignore, and readme files

---
+++ src/cfg-blank.py
@@ -0,0 +1,7 @@
+#cfg.py
+HOST = "irc.twitch.tv"                        # the Twitch IRC server
+PORT = 6667                                   # always use port 6667!
+NICK = # your Twitch username that will be the bot, lowercase
+PASS = # your Twitch OAuth token goes here
+CHAN = # the channel you want to join
+MODS = #list of mods for that channel (will be changed to auto include), format ['name1','name2','...']
